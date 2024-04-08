bd5036c481e919151e63e677124521c4933c5be9
joerivrij <joerivrijaldenhoven@gmail.com>
2024-04-08 14:12:48 +0200

    changes to aristophanes for better control of spans 3

--- aristophanes/comedy/tracer.go
+++ aristophanes/comedy/tracer.go
@@ -280,7 +280,7 @@ func (t *TraceServiceImpl) Span(ctx context.Context, spanRequest *pb.SpanRequest
 func (t *TraceServiceImpl) DatabaseSpan(ctx context.Context, spanRequest *pb.DatabaseSpanRequest) (*pb.TraceResponse, error) {
 	spanId := t.generateSpanID()
 
-	timeEndedStr := "2006-01-02T15:04:05.000Z"
+	timeEndedStr := time.Now().UTC().Format("2006-01-02'T'15:04:05.000")
 	timeEnded, err := time.Parse("2006-01-02T15:04:05.000Z", timeEndedStr)
 	if err != nil {
 		return nil, err
